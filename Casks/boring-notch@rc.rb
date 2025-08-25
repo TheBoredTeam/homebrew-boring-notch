b1ed930be79af418a059a1b45217f81ca1e70199
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-08-25 08:43:46 -0700

    Update version and SHA256 for RC 3

--- Casks/boring-notch@rc.rb
+++ Casks/boring-notch@rc.rb
@@ -1,6 +1,6 @@
 cask "boring-notch@rc" do
-  version "2.7-rc.2"
-  sha256 "d2b6a14165077bdffe96b1b2f233bd058431c0f1951b7f1e7b25c24ea18b7f05"
+  version "2.7-rc.3"
+  sha256 "853f6d8e405a6d730e524f2e864af1731b52cd191375f506678a62a63814bb81"
 
   url "https://github.com/TheBoredTeam/boring.notch/releases/download/v#{version}/boringNotch.dmg"
   name "Boring Notch"
