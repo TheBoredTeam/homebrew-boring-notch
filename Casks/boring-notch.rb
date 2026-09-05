24b807ccb854e68bca861fe3a0fda8585b05fb14
Tobias Alsema <tobias.alsema@uni-ulm.de>
2026-09-06 01:35:44 +0200

    fix: update deprecated postflight stanza

--- Casks/boring-notch.rb
+++ Casks/boring-notch.rb
@@ -2,9 +2,9 @@ cask "boring-notch" do
   version "2.7.3"
   sha256 "23786382548d33c59b309db558e513a92e3f95b63d611544dcddf5d196e46698"
 
-  url "https://github.com/TheBoredTeam/boring.notch/releases/download/v2.7.3/boringNotch.dmg"
+  url "https://github.com/TheBoredTeam/boring.notch/releases/download/v#{version}/boringNotch.dmg"
   name "Boring Notch"
-  desc "Not so boring notch That Rocks 🎸🎶 "
+  desc "Not so boring notch That Rocks"
   homepage "https://github.com/TheBoredTeam/boring.notch"
 
   livecheck do
@@ -17,18 +17,18 @@ cask "boring-notch" do
 
   app "boringNotch.app"
 
-  postflight do
-    app_path = appdir/"boringNotch.app"
-    next unless app_path.exist?
-
-    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", app_path]
+  postflight_steps do
+    if_path_exists "{{appdir}}/boringNotch.app" do
+      run "/usr/bin/xattr",
+          args: ["-dr", "com.apple.quarantine", "{{appdir}}/boringNotch.app"]
+    end
   end
 
   uninstall quit: "theboringteam.boringnotch"
 
   zap trash: [
     "~/Library/Application Scripts/theboringteam.boringnotch/",
-    "~/Library/Containers/theboringteam.boringnotch/",
     "~/Library/Containers/TheBoringNotch/",
+    "~/Library/Containers/theboringteam.boringnotch/",
   ]
 end
